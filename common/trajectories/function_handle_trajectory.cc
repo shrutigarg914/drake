87abcbabf02bacac9701e153fd5f53d404a0a64d
cohnt <cohnt@umich.edu>
2024-03-08 16:21:38 -0500

    wip

--- common/trajectories/function_handle_trajectory.cc
+++ common/trajectories/function_handle_trajectory.cc
@@ -1,5 +1,7 @@
 #include "drake/common/trajectories/function_handle_trajectory.h"
 
+#include "drake/math/jacobian.h"
+
 #include <utility>
 
 namespace drake {
@@ -41,10 +43,25 @@ template <typename T>
 MatrixX<T> FunctionHandleTrajectory<T>::DoEvalDerivative(
     const T& t, int derivative_order) const {
   const double eps = 1e-3;
+  // https://en.wikipedia.org/wiki/Five-point_stencil
+  // T t0 = std::max(start_time(), t - eps);
+  // T t1 = std::min(end_time(), t + eps);
+  // T dt = t1 - t0;
+  // DRAKE_THROW_UNLESS(dt > 0);
   if (derivative_order == 1) {
+    // return math::jacobian(func_, Eigen::Vector<double, 1>(static_cast<double>(t)));
+    // return math::jacobian(func_, Eigen::Vector<T, 1>(t));
+    // return math::jacobian(func_, t);
     return (value(t + eps) - value(t - eps)) / (2 * eps);
+    // return (value(t1) - value(t0)) / (dt);
+    // return (-value(t + 2 * eps) + 8 * value(t + eps) - 8 * value(t - eps) + value(t - 2 * eps)) / (12 * eps);
+  // } else if (derivative_order == 2) {
+  //   return (-value(t + 2 * eps) + 16 * value(t + eps) - 30 * value(t) + 16 * value(t - eps) - value(t - 2 * eps)) / (12 * eps * eps);
+  // } else if (derivative_order == 2) {
+  //   return math::hessian(func_, Eigen::Vector<T, 1>(t));
   } else {
     return (DoEvalDerivative(t + eps, derivative_order - 1) - DoEvalDerivative(t - eps, derivative_order - 1)) / (2 * eps);
+    // return (DoEvalDerivative(t1, derivative_order - 1) - DoEvalDerivative(t0, derivative_order - 1)) / (dt);
   }
 }
 
